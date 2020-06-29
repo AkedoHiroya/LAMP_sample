3073b081642888378706e22c5f797f1e1a1592d6
githubAkedoHiroya <h.akedo.sys19@morijyobi.ac.jp>
2020-06-29 12:09:01 +0900

    a

--- index.php
+++ index.php
@@ -45,6 +45,12 @@ try {
         </div>
     </div>
 
+    <?php if($_GET['fg'] = 1){ ?>
+        <p>success</p>
+    <?php }else if($_GET['fg']== 2){ ?>
+        <p>failed</p>
+    <?php } ?>
+
     <div class="container">
         <ul class="nav nav-tabs">
             <li class="nav-item">
